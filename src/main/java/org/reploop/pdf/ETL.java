398cb760da160072be1f643649a980eb7dbfb6b1
曹张治 <zhangzhicao@huangbaoche.com>
2020-07-27 15:32:51 +0800

    test some case

---
+++ src/main/java/org/reploop/pdf/ETL.java
@@ -0,0 +1,35 @@
+package org.reploop.pdf;
+
+import com.google.common.base.Strings;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ETL {
+
+    public static void main(String... args) throws IOException {
+        //Path dir = Paths.get("/Users/george/reports/2019-Q1/wiki");
+        Path dir = Paths.get("/Users/george/Downloads");
+
+        read(dir.resolve("Patterns Principles and Practices of Domain-Driven Design.pdf"));
+
+    }
+
+    public static void read(Path path) throws IOException {
+        PdfReader reader = new PdfReader(path.toUri().toURL());
+        int pages = reader.getNumberOfPages();
+        for (int i = 1; i <= pages; i++) {
+            PdfDictionary pn =reader.getPageN(i);
+            String text = PdfTextExtractor.getTextFromPage(reader, i);
+            System.out.println(text);
+            if (Strings.isNullOrEmpty(text)) {
+                continue;
+            }
+            break;
+        }
+    }
+}
